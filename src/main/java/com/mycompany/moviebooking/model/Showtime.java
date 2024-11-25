051250bcda01b1499f70faf7191bfcc3585d1e9f
GaruVA <garukaassalaarachchi@gmail.com>
2024-11-25 19:14:28 +0530

    booking selection - functional test file for showtimes

--- src/main/java/com/mycompany/moviebooking/model/Showtime.java
+++ src/main/java/com/mycompany/moviebooking/model/Showtime.java
@@ -1,64 +1,91 @@
 package com.mycompany.moviebooking.model;
 
-import java.util.Date;
-import java.sql.Time;
-
 public class Showtime {
-    private int id;
+    private int showtimeId;
     private int movieId;
+    private String movieTitle;
     private int theatreId;
-    private Date showDate;
-    private Time showTime;
+    private String theatreName;
+    private String showDate;
+    private String showTime;
     private int availableSeats;
-    private Movie movie;
-    private Theatre theatre;
 
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getMovieId() {
-        return movieId;
-    }
-    public void setMovieId(int movieId) {
+    // Constructor
+    public Showtime(int showtimeId, int movieId, String movieTitle, int theatreId, String theatreName,
+                    String showDate, String showTime, int availableSeats) {
+        this.showtimeId = showtimeId;
         this.movieId = movieId;
-    }
-    public int getTheatreId() {
-        return theatreId;
-    }
-    public void setTheatreId(int theatreId) {
+        this.movieTitle = movieTitle;
         this.theatreId = theatreId;
-    }
-    public Date getShowDate() {
-        return showDate;
-    }
-    public void setShowDate(Date showDate) {
+        this.theatreName = theatreName;
         this.showDate = showDate;
-    }
-    public Time getShowTime() {
-        return showTime;
-    }
-    public void setShowTime(Time showTime) {
         this.showTime = showTime;
-    }
-    public int getAvailableSeats() {
-        return availableSeats;
-    }
-    public void setAvailableSeats(int availableSeats) {
         this.availableSeats = availableSeats;
     }
 
-    public void setTheatre(Theatre theatre) {
-        this.theatre = theatre;    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
+    // Getters
+    public int getShowtimeId() {
+        return showtimeId;
     }
 
-    public void setShowtimeId(int id) {
-        this.id = id;
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public int getTheatreId() {
+        return theatreId;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public String getShowDate() {
+        return showDate;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    // Setters
+    public void setShowtimeId(int showtimeId) {
+        this.showtimeId = showtimeId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public void setTheatreId(int theatreId) {
+        this.theatreId = theatreId;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+
+    public void setShowDate(String showDate) {
+        this.showDate = showDate;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
     }
 }
